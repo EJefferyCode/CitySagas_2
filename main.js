14c902d0b585e709465f78c575a4af63113def1e
EJefferyCode <emmyjeffery99@gmail.com>
2017-03-19 13:46:48 -0400

    second version

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#img").click(function(){
+        $("header nav ul li").slideToggle(300);
+    });
+});
+
+$(document).ready(function(){
+    $("#button").click(function(){
+        $("#fullMenu").slideToggle(300);
+    });
+});
